1ce09d6c217f2e8a31a2e74dd5fda258ad626411
Johan Tique <johan.tique@analisistem.com>
2020-07-08 09:39:35 -0500

    022

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -15,4 +15,34 @@ RSpec.describe "Tasks", type: :request do
       expect(response).to render_template(:new)
     end
   end
+  describe "POST /tasks" do
+    let(:category) { create :category }
+    let(:participant) { create :user }
+    let(:params) do
+      {
+        "task"=>{
+          "name"=>"test 7",
+          "due_date"=> Date.today + 5.days,
+          "category_id"=> category.id.to_s,
+          "description"=>"test",
+          "participating_users_attributes" => {
+            "0"=>{
+              "user_id"=> participant.id.to_s,
+              "role"=>"1",
+              "_destroy"=>"false"
+              }
+            }
+          }
+      }
+    end
+
+    it 'creates a new task and redirect to show page' do
+      post '/tasks', params: params
+
+      expect(response).to redirect_to(assigns(:task))
+      follow_redirect!
+      expect(response).to render_template(:show)
+      expect(response.body).to include('Task was successfully created.')
+    end
+  end
 end
