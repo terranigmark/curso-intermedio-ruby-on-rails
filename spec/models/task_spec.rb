c9264cc2f1f6a06897be300e777360e3baeb440e
Johan Tique <johan.tique@analisistem.com>
2020-07-08 08:52:45 -0500

    020

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -16,19 +16,54 @@ RSpec.describe Task, type: :model do
   it { is_expected.to validate_uniqueness_of(:name) }
 
   describe '#save' do
-    let(:participants_count) { 4 }
-    subject(:task) { build(:task_with_participants, participants_count: participants_count) }
+    context 'with params from scratch' do
+      let(:owner) { create :user }
+      let(:category) { create :category }
+      let(:participant_1) { build :participant, :responsible }
+      let(:participant_2) { build :participant, :follower }
 
-    it 'is persisted' do
-      expect(task.save).to eq true
+      subject(:task) do
+        described_class.new(
+          name: 'Mi tarea',
+          description: 'desc',
+          due_date: Date.today + 5.days,
+          category: category,
+          owner: owner,
+          participating_users: [participant_1, participant_2]
+        )
+      end
+
+      it { is_expected.to be_valid }
+      context 'after save' do
+        before(:each) { task.save }
+
+        it { is_expected.to be_persisted }
+
+        it 'has a computed code' do
+          expect(task.code).to be_present
+        end
+      end
+      context 'with due_date in the past' do
+        subject { task.tap { |t| t.due_date = Date.today - 1.day } }
+
+        it { is_expected.to_not be_valid }
+      end
     end
+    context 'with params from FactoryBot' do
+      let(:participants_count) { 4 }
+      subject(:task) { build(:task_with_participants, participants_count: participants_count) }
 
-    context 'after save' do
-      before(:each) { task.save }
+      it 'is persisted' do
+        expect(task.save).to eq true
+      end
 
-      it 'has all associated participants' do
-        expect(task.participating_users.count).to eq participants_count
-        expect(Participant.count).to eq participants_count
+      context 'after save' do
+        before(:each) { task.save }
+
+        it 'has all associated participants' do
+          expect(task.participating_users.count).to eq participants_count
+          expect(Participant.count).to eq participants_count
+        end
       end
     end
   end
