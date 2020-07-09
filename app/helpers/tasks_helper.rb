ce4e8bc6fb3cd88a03d224fb4ecd0b76f5cd4d41
Johan Tique <johan.tique@analisistem.com>
2020-07-09 12:11:33 -0500

    034

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,5 @@
 module TasksHelper
+  def available_events_for(task)
+    task.aasm.permitted_transitions.map { |t| t[:event] }
+  end
 end
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -55,7 +55,7 @@ RSpec.describe "Tasks", type: :request do
     it 'updates the state' do
       task.save
 
-      patch trigger_task_path(task, event: event)
+      patch trigger_task_path(task, event: event, format: :js)
 
       expect(task.reload.status).to eq 'in_process'
     end
