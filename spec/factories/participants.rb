ce911cf25d703a69b74a10cd05348607b70bb585
Johan Tique <johan.tique@analisistem.com>
2020-07-08 20:09:19 -0500

    027

--- spec/factories/participants.rb
+++ spec/factories/participants.rb
@@ -8,5 +8,9 @@ FactoryBot.define do
     trait :follower do
       role { Participant::ROLES[:follower] }
     end
+
+    after(:build) do |participant, _|
+      participant.user.save
+    end
   end
 end
